1b863c64ea07a983da276a92b63cc5e1112e04a1
rellis14 <rellis14@myseneca.ca>
2022-06-11 14:50:34 -0400

    lab5 update

--- Lab1.py
+++ Lab1.py
@@ -5,6 +5,10 @@ def calcAge(birthdate):
     currentdate = date.today().year
     return int(currentdate) - int(birthdate)
 
+def helloWorld():
+	print('Hello World')
+
 if __name__ == '__main__':
     born = input("Please enter the year you were born: ")
     print("You are " + str(calcAge(born)) + " years old.")
+	helloWorld()
